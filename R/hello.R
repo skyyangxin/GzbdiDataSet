59aabac27a31a0f994acab11ede3732f84625ca6
yangxin_xin <1325542390@qq.com>
2020-02-10 20:22:34 +0800

    1

--- R/hello.R
+++ R/hello.R
@@ -14,10 +14,10 @@
 #   Test Package:              'Ctrl + Shift + T'
 
 #获取数据集
-# @title getDataSet
-# @return da
-# @export
-# @author yx
+#' @title getDataSet
+#' @return da
+#' @export
+#' @author yx
 getDataSet <- function(){
   #  读取api
   res <- GET("http://39.108.91.24:8099/query")
@@ -34,10 +34,10 @@ getDataSet <- function(){
 }
 
 #导出excel
-# @title exportExcel
-# @return String
-# @export
-# @author yx
+#' @title exportExcel
+#' @return String
+#' @export
+#' @author yx
 exportExcel <- function(data){
   url <- getwd()
   write.csv(data,file=paste(url,"data.csv",sep = '/'))
@@ -49,10 +49,10 @@ exportExcel <- function(data){
 # item：列名（如data中time）
 # name: 列名描述
 # 获取柱状图
-# @title getHistogram
-# @return String
-# @export
-# @author hm
+#' @title getHistogram
+#' @return String
+#' @export
+#' @author hm
 getHistogram <- function(data,province){
   x<-data
 
@@ -61,15 +61,17 @@ getHistogram <- function(data,province){
   ggplot(t, aes(format(as.Date(time),format="%m-%d"), as.numeric(confirmed_num))) +
     geom_col(fill='firebrick') + theme_minimal(base_size = 14) +
     xlab(NULL) + ylab(NULL)+ggtitle(paste(province,'确诊数柱状图'))
+  return("画图成功")
 }
 
-# @title initLib
-# @return Object
-# @export
-# @author yx
+#' @title initLib
+#' @return String
+#' @export
+#' @author yx
 initLib <- function(){
   library(httr)
   library(rjson)
   library(remotes)
   library(ggplot2)
+  return("包加载成功")
 }

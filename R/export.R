df94b6b229c51048f9e70f7a721cc0c4499895c8
yangxin_xin <1325542390@qq.com>
2020-02-10 17:41:28 +0800

    1

--- R/export.R
+++
@@ -1,5 +0,0 @@
-exportExcel <- function(data){
-  url <- getwd()
-  write.csv(data,file=paste(url,"data.csv",sep = '/'))
-  return('导出成功')
-}
--- R/hello.R
+++ R/hello.R
@@ -29,3 +29,8 @@ getDataSet <- function(){
 }
 
 
+exportExcel <- function(data){
+  url <- getwd()
+  write.csv(data,file=paste(url,"data.csv",sep = '/'))
+  return('导出成功')
+}

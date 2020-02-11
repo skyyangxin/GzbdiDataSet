a403c9e330933a09d8c84553810919da453bb656
yangxin_xin <1325542390@qq.com>
2020-02-11 14:41:08 +0800

    下载excel列名加入解释

--- R/hello.R
+++ R/hello.R
@@ -39,6 +39,8 @@ getDataSet <- function(){
 #' @export
 #' @author yx
 exportExcel <- function(data){
+  columnName <- names(data)
+  names(data) <- editColumnName(columnName)
   url <- getwd()
   write.csv(data,file=paste(url,"data.csv",sep = '/'))
   print(paste("文件保存地址：",url,"/data.csv",sep = "",collapse = ""))
@@ -135,3 +137,22 @@ getLine <- function(data,colume_name='confirmed_num',address='全部',startTime=
   gg+geom_line()+xlab("时间")+ylab(desc)+ggtitle(paste(address,desc,"折线图",sep = "",collapse = ""))
 }
 
+# 列名添加解释
+editColumnName <- function(columnName){
+  newColumnName <- c()
+  for (i in columnName) {
+    name <- switch (i,
+      "province" = "province 地区",
+      "remove_observation_num" = "remove_observation_num 解除医学观察数",
+      "touch_num" = "touch_num 密切接触者数",
+      "confirmed_num" = "confirmed_num 累计确诊数",
+      "severe_num" = "severe_num 现有重症数",
+      "accept_num" = "accept_num 接受医学观察数",
+      "time" = "time 发布日期",
+      "cure_num" = "cure_num 累计治愈数",
+      "die_num" = "die_num 累计死亡数"
+    )
+    newColumnName[length(newColumnName)+1] <- name
+  }
+  return(newColumnName)
+}
